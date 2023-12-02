2b949236c9fb7990c00b1c87d42ab16472c2e4b3
pavel <ppashchenkov@gmail.com>
2023-12-02 15:52:38 +0300

    edit name in lesson_1_task_7

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,32 +1,32 @@
-def print_to_consoce_1():
+def print_to_console_1():
     print(1,end='')
-def print_to_consoce_2():
+def print_to_console_2():
     print(2,end='')
-def print_to_consoce_3():
+def print_to_console_3():
     print(3,end='')
-def print_to_consoce_4():
+def print_to_console_4():
     print(4,end='')
-def print_to_consoce_5():
+def print_to_console_5():
     print(5,end='')
-def print_to_consoce_6():
+def print_to_console_6():
     print(6,end='')
-def print_to_consoce_7():
+def print_to_console_7():
     print(7,end='')
-def print_to_consoce_8():
+def print_to_console_8():
     print(8,end='')
-def print_to_consoce_9():
+def print_to_console_9():
     print(9,end='')
-def print_to_consoce_0():
+def print_to_console_0():
     print(0,end='')
 
-print_to_consoce_8()
-print_to_consoce_8()
-print_to_consoce_0()
-print_to_consoce_0()
-print_to_consoce_5()
-print_to_consoce_5()
-print_to_consoce_5()
-print_to_consoce_3()
-print_to_consoce_5()
-print_to_consoce_3()
-print_to_consoce_5()
+print_to_console_8()
+print_to_console_8()
+print_to_console_0()
+print_to_console_0()
+print_to_console_5()
+print_to_console_5()
+print_to_console_5()
+print_to_console_3()
+print_to_console_5()
+print_to_console_3()
+print_to_console_5()
