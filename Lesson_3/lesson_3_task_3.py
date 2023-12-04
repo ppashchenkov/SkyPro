876c99d7b162682c1047a11ca328b3f0cd583c70
ppashchenkov <ppashchenkov@gmail.com>
2023-12-04 10:33:35 +0300

    Lesson 3, refactoring task 3

--- Lesson_3/lesson_3_task_3.py
+++ Lesson_3/lesson_3_task_3.py
@@ -5,6 +5,6 @@ to_address = Address(101003, "Moscow", "Gorky str.", "120", "23")
 from_address = Address(125130, "Moscow", "Mitinskaya str.", "19", "3")
 my_mail = Mailing(to_address, from_address, 150, "12544806")
 
-print("Отправление ", my_mail.track," из ", my_mail.from_address.index,", ", my_mail.from_address.city, ", ", my_mail.from_address.street, ", "
-      , my_mail.from_address.house, " - ", my_mail.from_address.appartment, " в ", my_mail.to_address.index, ", ", my_mail.to_address.city, ", ", my_mail.to_address.street
-      ,", ", my_mail.to_address.house," - ", my_mail.to_address.appartment,". Стоимость ", my_mail.cost," рублей.", sep="")
+print(f"Отправление {my_mail.track} из {my_mail.from_address.index}, {my_mail.from_address.city}, {my_mail.from_address.street}"
+      f", {my_mail.from_address.house} - {my_mail.from_address.appartment} в {my_mail.to_address.index}, {my_mail.to_address.city}"
+      f", {my_mail.to_address.street}, {my_mail.to_address.house} - {my_mail.to_address.appartment}. Стоимость {my_mail.cost} рублей.")
