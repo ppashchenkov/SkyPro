1e384671e89c066cdca6222003bc9cb7349b8ff9
ppashchenkov <ppashchenkov@gmail.com>
2023-12-04 10:21:50 +0300

    Lesson 3, refactoring task 3

--- Lesson_3/address.py
+++ Lesson_3/address.py
@@ -1,9 +1,4 @@
 class Address:
-    index = 0
-    city = "no"
-    street = "no"
-    house = "0"
-    appartment = "0"
 
     def __init__(self, _index, _city, _street, _house, _appartment):
         self.index = _index
