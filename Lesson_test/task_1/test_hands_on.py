9982ffa758ee9957b91bcff00600096cd9e7717a
ppashchenkov <ppashchenkov@gmail.com>
2023-12-11 11:01:07 +0300

    Lesson test Refactoring task_1

--- Lesson_test/task_1/test_hands_on.py
+++ Lesson_test/task_1/test_hands_on.py
@@ -7,101 +7,28 @@ my_data = HandsData("Иван", "Петров", "Ленина, 55-3", "test@skyp
                     "Россия", "QA", "SkyPro")
 
 
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(132, 32, 41, 1)")
-    ])
-def test_hands_on_zip(my_data_, res_):
+@pytest.mark.parametrize('my_data_, red_, green_', [
+    (my_data, "rgba(132, 32, 41, 1)", "rgba(15, 81, 50, 1)")
+])
+def test_hands_on_zip(my_data_, red_, green_):
+    green_list = ['first_color', 'last_color', 'address_color', 'city_color', 'country_color',
+                  'email_color', 'phone_color', 'job_color', 'company_color']
+    green_result = True
+
     web_data = hands_on(my_data)
-    res = web_data[4].find_element(By.CSS_SELECTOR, "#zip-code").value_of_css_property("color")
 
-    assert res == res_
+    zip_color = web_data[4].find_element(By.CSS_SELECTOR, "#zip-code").value_of_css_property("color")
+    green_list[0] = web_data[3].find_element(By.CSS_SELECTOR, "#first-name").value_of_css_property("color")
+    green_list[1] = web_data[3].find_element(By.CSS_SELECTOR, "#last-name").value_of_css_property("color")
+    green_list[2] = web_data[4].find_element(By.CSS_SELECTOR, "#address").value_of_css_property("color")
+    green_list[3] = web_data[4].find_element(By.CSS_SELECTOR, "#city").value_of_css_property("color")
+    green_list[4] = web_data[4].find_element(By.CSS_SELECTOR, "#country").value_of_css_property("color")
+    green_list[5] = web_data[5].find_element(By.CSS_SELECTOR, "#e-mail").value_of_css_property("color")
+    green_list[6] = web_data[5].find_element(By.CSS_SELECTOR, "#phone").value_of_css_property("color")
+    green_list[7] = web_data[6].find_element(By.CSS_SELECTOR, "#job-position").value_of_css_property("color")
+    green_list[8] = web_data[6].find_element(By.CSS_SELECTOR, "#company").value_of_css_property("color")
 
+    for green in green_list:
+        if green != green_: green_result = False
 
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_first_name(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[3].find_element(By.CSS_SELECTOR, "#first-name").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_last_name(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[3].find_element(By.CSS_SELECTOR, "#last-name").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_address(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[4].find_element(By.CSS_SELECTOR, "#address").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_city(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[4].find_element(By.CSS_SELECTOR, "#city").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_country(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[4].find_element(By.CSS_SELECTOR, "#country").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_email(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[5].find_element(By.CSS_SELECTOR, "#e-mail").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_phone(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[5].find_element(By.CSS_SELECTOR, "#phone").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_job(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[6].find_element(By.CSS_SELECTOR, "#job-position").value_of_css_property("color")
-
-    assert res == res_
-
-
-@pytest.mark.parametrize('my_data_, res_', [
-    (my_data, "rgba(15, 81, 50, 1)")
-    ])
-def test_hands_on_company(my_data_, res_):
-    web_data = hands_on(my_data)
-    res = web_data[6].find_element(By.CSS_SELECTOR, "#company").value_of_css_property("color")
-
-    assert res == res_
+    assert zip_color == red_ and green_result
