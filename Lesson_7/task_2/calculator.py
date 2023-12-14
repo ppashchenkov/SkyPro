317e81023c98592fb6c9565bf78ec6d2db001ce8
ppashchenkov <ppashchenkov@gmail.com>
2023-12-14 23:07:26 +0300

    lesson_7 Done

--- Lesson_7/task_2/calculator.py
+++ Lesson_7/task_2/calculator.py
@@ -4,10 +4,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 from main_page import MainPage
 
 
-def calculator(delay_):
+def calculator(_delay):
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     main_page = MainPage(driver)
     main_page.set_delay()
     main_page.get_7_plus_8()
-    return main_page.get_result(delay_)
+    return main_page.get_result(_delay)
 
